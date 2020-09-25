36122645b27d81cb586bde1d73e523c234046e9d
Roger Cheng <roger.random@outlook.com>
2020-09-25 01:49:29 -0700

    Brief experiment, couldn't get it to work well.

--- ESP32_Stepper/src/main.cpp
+++ ESP32_Stepper/src/main.cpp
@@ -1,9 +1,26 @@
 #include <Arduino.h>
+#include <ESPStepperMotorServer.h>
 
-void setup() {
-  // put your setup code here, to run once:
+ESPStepperMotorServer *stepperMotorServer;
+
+const char *wifiName= "<don't commit to github>"; // enter the SSID of the wifi network to connect to
+const char *wifiSecret = "<don't commit to github>"; // enter the password of the the existing wifi network here
+
+void setup() 
+{
+  Serial.begin(115200);
+  stepperMotorServer = new ESPStepperMotorServer(ESPServerRestApiEnabled | ESPServerWebserverEnabled | ESPServerSerialEnabled);
+  stepperMotorServer->setWifiCredentials(wifiName, wifiSecret);
+  stepperMotorServer->setWifiMode(ESPServerWifiModeClient); //start the server as a wifi client (DHCP client of an existing wifi network)
+
+  ESPStepperMotorServer_StepperConfiguration* config = 
+    new ESPStepperMotorServer_StepperConfiguration(12, 14);
+
+  config->setDisplayName("Motor1");
+  stepperMotorServer->addOrUpdateStepper(config, 0);
+  stepperMotorServer->start();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
 }
