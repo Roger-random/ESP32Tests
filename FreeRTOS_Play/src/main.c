ff147f22d0fdfa1da28af1c53a33a138774d319b
Roger Cheng <roger.random@outlook.com>
2021-01-10 18:48:27 -0800

    Blink LED with loop

--- FreeRTOS_Play/src/main.c
+++ FreeRTOS_Play/src/main.c
@@ -5,15 +5,30 @@
 //
 // Copyright (c) Roger Cheng and released "AS IS" under MIT License
 
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "driver/gpio.h"
+
+static const gpio_num_t led_pin = GPIO_NUM_14;
 
 void app_main()
 {
+  // Configure a pin for GPIO output to blink an LED
+  gpio_config_t io_conf = {
+      .mode = GPIO_MODE_OUTPUT,
+      .intr_type = GPIO_INTR_DISABLE,
+      .pull_down_en = 0,
+      .pull_up_en = 0,
+      .pin_bit_mask = (1ULL<<led_pin),
+  };
+  gpio_config(&io_conf);
+
+  // Blink that LED
+  bool led_on = true;
   while(1)
   {
-    printf("Verify build tool chain works\n");
+    gpio_set_level(led_pin, led_on);
     vTaskDelay(pdMS_TO_TICKS(1000));
+    led_on = !led_on;
   }
 }
