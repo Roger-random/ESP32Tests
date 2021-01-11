94f3843a5c21699f7161aa2e16d0fbfef76b94b1
Roger Cheng <roger.random@outlook.com>
2021-01-10 18:33:41 -0800

    Verify build tool chain works

--- FreeRTOS_Play/src/main.c
+++ FreeRTOS_Play/src/main.c
@@ -1,1 +1,19 @@
-void app_main() {}
+// By default, ESP32 runs a derivative of FreeRTOS to organize both developer
+// code and background housekeeping work. Our apps can participate in this
+// system as well, so this project serves as a playground for learning and
+// experimentation of FreeRTOS API.
+//
+// Copyright (c) Roger Cheng and released "AS IS" under MIT License
+
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+void app_main()
+{
+  while(1)
+  {
+    printf("Verify build tool chain works\n");
+    vTaskDelay(pdMS_TO_TICKS(1000));
+  }
+}
