dc15289cb6f91b657c6081fb5bc7601426bc31d7
Roger Cheng <roger.random@outlook.com>
2021-03-11 23:47:55 -0800

    Reusing joystick code from ESP32 Sawppy

--- MCPWM_DutyCycle/src/main.c
+++ MCPWM_DutyCycle/src/main.c
@@ -1,1 +1,20 @@
-void app_main() {}
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
+
+#include <joy_adc.h>
+#include <joy_msg.h>
+
+void app_main()
+{
+  QueueHandle_t xJoystickQueue = xQueueCreate(1, sizeof(joy_msg));
+
+  if (NULL == xJoystickQueue)
+  {
+    printf("ERROR: Queue allocation failed.\n");
+  }
+  else
+  {
+    xTaskCreate(joy_adc_read_task, "joy_adc_read_task", 2048, xJoystickQueue, 20, NULL);
+  }
+};
