e078efc21298b901ded47508921ec952cd8cdce2
Roger Cheng <roger.random@outlook.com>
2021-03-12 01:35:14 -0800

    Joystick as D-pad

--- MCPWM_DutyCycle/src/duty_cycle.c
+++ MCPWM_DutyCycle/src/duty_cycle.c
@@ -1,9 +1,29 @@
 #include "duty_cycle.h"
 
+void update_duty_cycle(float velocity, mcpwm_motor_control MC)
+{
+  if (velocity > 0)
+  {
+    mcpwm_set_duty(MC.unit, MC.timer, MCPWM_GEN_A, velocity);
+    mcpwm_set_duty(MC.unit, MC.timer, MCPWM_GEN_B, 0);
+  }
+  else if (velocity < 0)
+  {
+    mcpwm_set_duty(MC.unit, MC.timer, MCPWM_GEN_A, 0);
+    mcpwm_set_duty(MC.unit, MC.timer, MCPWM_GEN_B, fabs(velocity));
+  }
+  else
+  {
+    mcpwm_set_duty(MC.unit, MC.timer, MCPWM_GEN_A, 0);
+    mcpwm_set_duty(MC.unit, MC.timer, MCPWM_GEN_B, 0);
+  }
+}
+
 void duty_cycle_task(void* pvParam)
 {
   joy_msg message;
 
+  // Retrieve parameter: joystick message queue
   if (NULL == pvParam)
   {
     printf("ERROR: duty_cycle_task expected handle to joy_msg queue.\n");
@@ -11,20 +31,126 @@ void duty_cycle_task(void* pvParam)
   }
   QueueHandle_t xJoystickQueue = (QueueHandle_t)pvParam;
 
+  // Initialize MCPWM peripherals
+  float duty_cycle = 0.0;
+  mcpwm_config_t mcpwm_config = {
+    .frequency = pwm_freq,
+    .cmpr_a = 0,
+    .cmpr_b = 0,
+    .counter_mode = MCPWM_UP_COUNTER,
+    .duty_mode = MCPWM_DUTY_MODE_0,
+  };
+  for (int wheel = 0; wheel < wheel_count; wheel++)
+  {
+    mcpwm_init(
+      speed_control[wheel].unit,
+      speed_control[wheel].timer,
+      &mcpwm_config);
+    mcpwm_gpio_init(
+      speed_control[wheel].unit,
+      speed_control[wheel].signalA,
+      speed_control[wheel].gpioA);
+    mcpwm_gpio_init(
+      speed_control[wheel].unit,
+      speed_control[wheel].signalB,
+      speed_control[wheel].gpioB);
+    update_duty_cycle(duty_cycle, speed_control[wheel]);
+  }
+
+  // Enter interactive loop
   bool bWaitingForPress = true;
+  bool bWaitingForCenter = false;
+  bool bMotorOn = false;
+  bool bUpdated = false;
+  float fIncrement = 1.0;
   while(true)
   {
+    bUpdated = false;
     if (pdTRUE == xQueueReceive(xJoystickQueue, &message, portMAX_DELAY))
     {
       if (bWaitingForPress && message.buttons[button_mode])
       {
         bWaitingForPress = false;
-        printf("Wax On!\n");
+        if (bMotorOn)
+        {
+          bMotorOn = false;
+        }
+        else
+        {
+          bMotorOn = true;
+        }
+        bUpdated = true;
       }
       else if (!bWaitingForPress && !message.buttons[button_mode])
       {
         bWaitingForPress = true;
-        printf("Wax Off!\n");
+      }
+
+      if (bWaitingForCenter &&
+        fabs(message.axes[axis_speed]) < center_threshold &&
+        fabs(message.axes[axis_steer]) < center_threshold)
+      {
+        bWaitingForCenter = false;
+      }
+      else if (!bWaitingForCenter &&
+        fabs(message.axes[axis_speed]) > center_threshold)
+      {
+        bWaitingForCenter = true;
+        if (message.axes[axis_speed] > 0)
+        {
+          duty_cycle += fIncrement;
+          if (duty_cycle > 100.0)
+          {
+            duty_cycle = 100.0;
+          }
+        }
+        else
+        {
+          duty_cycle -= fIncrement;
+          if (duty_cycle < -100.0)
+          {
+            duty_cycle = -100.0;
+          }
+        }
+        bUpdated = true;
+      }
+      else if (!bWaitingForCenter &&
+        fabs(message.axes[axis_steer]) > center_threshold)
+      {
+        bWaitingForCenter = true;
+        if (message.axes[axis_steer] > 0)
+        {
+          fIncrement *= 10;
+          if (fIncrement > 10.0)
+          {
+            fIncrement = 10;
+          }
+        }
+        else
+        {
+          fIncrement /= 10;
+          if (fIncrement < 0.1)
+          {
+            fIncrement = 0.1;
+          }
+        }
+        bUpdated = true;
+      }
+
+      if (bUpdated)
+      {
+        printf("Motor %d increment %.1f duty %.1f\n", bMotorOn, fIncrement, duty_cycle);
+        for (int wheel = 0; wheel < wheel_count; wheel++)
+        {
+          if (bMotorOn)
+          {
+            update_duty_cycle(duty_cycle, speed_control[wheel]);
+          }
+          else
+          {
+            update_duty_cycle(0, speed_control[wheel]);
+          }
+        }
       }
     }
     else
--- MCPWM_DutyCycle/src/duty_cycle.h
+++ MCPWM_DutyCycle/src/duty_cycle.h
@@ -9,12 +9,92 @@
 #ifndef INC_DUTY_CYCLE_H
 #define INC_DUTY_CYCLE_H
 
+#include "math.h"
+
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 #include "freertos/task.h"
 
 #include "joy_msg.h"
 #include "driver/mcpwm.h"
+
+#define wheel_count 6
+
+static const uint32_t pwm_freq = 20000;
+
+static const float center_threshold = 0.5;
+
+/*
+ * @brief MCPWM peripheral parameters for a particular wheel
+ */
+typedef struct mcpwm_motor_control {
+  mcpwm_unit_t unit;
+  mcpwm_timer_t timer;
+  mcpwm_io_signals_t signalA;
+  gpio_num_t gpioA;
+  mcpwm_io_signals_t signalB;
+  gpio_num_t gpioB;
+} mcpwm_motor_control;
+
+/*
+ * @brief MCPWM peripheral parameters for all wheels
+ */
+static const mcpwm_motor_control speed_control[wheel_count] = {
+  // front_left
+  {
+    MCPWM_UNIT_0,
+    MCPWM_TIMER_0,
+    MCPWM0A,
+    GPIO_NUM_32,
+    MCPWM0B,
+    GPIO_NUM_33,
+  },
+  // front_right
+  {
+    MCPWM_UNIT_1,
+    MCPWM_TIMER_0,
+    MCPWM0A,
+    GPIO_NUM_19,
+    MCPWM0B,
+    GPIO_NUM_18,
+  },
+  // mid_left
+  {
+    MCPWM_UNIT_0,
+    MCPWM_TIMER_1,
+    MCPWM1A,
+    GPIO_NUM_25,
+    MCPWM1B,
+    GPIO_NUM_26,
+  },
+  // mid_right
+  {
+    MCPWM_UNIT_1,
+    MCPWM_TIMER_1,
+    MCPWM1A,
+    GPIO_NUM_17,
+    MCPWM1B,
+    GPIO_NUM_16,
+  },
+  // back_left
+  {
+    MCPWM_UNIT_0,
+    MCPWM_TIMER_2,
+    MCPWM2A,
+    GPIO_NUM_27,
+    MCPWM2B,
+    GPIO_NUM_12,
+  },
+  // back_right
+  {
+    MCPWM_UNIT_1,
+    MCPWM_TIMER_2,
+    MCPWM2A,
+    GPIO_NUM_4,
+    MCPWM2B,
+    GPIO_NUM_2,
+  },
+};
 
 /*
  * @brief FreeRTOS task for interactively exploring MCPWM duty cycle
