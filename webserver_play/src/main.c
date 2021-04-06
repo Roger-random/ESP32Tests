ff8d25fa36b52464e7d3a728cc77c90c21f29e77
Roger Cheng <roger.random@outlook.com>
2021-04-05 18:30:22 -0700

    Copied Espressif station example

--- webserver_play/src/main.c
+++ webserver_play/src/main.c
@@ -7,5 +7,5 @@
 
 void app_main()
 {
-  xTaskCreate(station_start_task, "station_start_task", 2048, NULL, 20, NULL);
+  xTaskCreate(station_start_task, "station_start_task", 1024*8, NULL, 20, NULL);
 }
---
+++ webserver_play/src/secrets.h
@@ -0,0 +1,11 @@
+/*
+ * Header file for all the stuff that shouldn't be publicly visible
+ * on GitHub. After pulling this file down, run the following
+ * command so further updates are ignored:
+ *
+ * git update-index --skip-worktree secrets.h
+ *
+ */
+
+#define EXAMPLE_ESP_WIFI_SSID "Your WiFi SSID here"
+#define EXAMPLE_ESP_WIFI_PASS "Your WiFi password here"
