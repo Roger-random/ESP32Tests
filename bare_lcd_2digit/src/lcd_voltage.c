287f200c439f65685e2993cd19f589fb9820481b
Regorlas <roger.random@outlook.com>
2022-05-04 10:54:46 -0700

    Hard coded 42

--- bare_lcd_2digit/src/lcd_voltage.c
+++ bare_lcd_2digit/src/lcd_voltage.c
@@ -2,6 +2,10 @@
 
 void lcd_voltage_task(void *arg)
 {
+    uint8_t duty_high = 31;
+    uint8_t duty_mid = duty_high/2;
+    uint8_t duty_low = 0;
+
     ledc_timer_config_t ledc_timer = {
         .speed_mode         = LEDC_HIGH_SPEED_MODE,
         .timer_num          = LEDC_TIMER_0,
@@ -11,30 +15,158 @@ void lcd_voltage_task(void *arg)
     };
     ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
 
+    ledc_timer.speed_mode = LEDC_LOW_SPEED_MODE;
+    ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
+
     ledc_channel_config_t ledc_channel = {
         .speed_mode = LEDC_HIGH_SPEED_MODE,
         .channel    = LEDC_CHANNEL_0,
         .timer_sel  = LEDC_TIMER_0,
         .intr_type  = LEDC_INTR_DISABLE,
-        .gpio_num   = seg_1,
-        .duty       = 15,
+        .gpio_num   = seg_0,
+        .duty       = duty_mid,
         .hpoint     = 0
     };
     ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
 
+    ledc_channel.channel = LEDC_CHANNEL_1;
+    ledc_channel.gpio_num = seg_1;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.channel = LEDC_CHANNEL_2;
+    ledc_channel.gpio_num = seg_2;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.channel = LEDC_CHANNEL_3;
+    ledc_channel.gpio_num = seg_3;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.channel = LEDC_CHANNEL_4;
+    ledc_channel.gpio_num = seg_4;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.channel = LEDC_CHANNEL_5;
+    ledc_channel.gpio_num = seg_5;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.channel = LEDC_CHANNEL_6;
+    ledc_channel.gpio_num = seg_6;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.channel = LEDC_CHANNEL_7;
+    ledc_channel.gpio_num = seg_7;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.speed_mode = LEDC_LOW_SPEED_MODE;
+
+    ledc_channel.channel = LEDC_CHANNEL_0;
+    ledc_channel.gpio_num = com_0;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    ledc_channel.channel = LEDC_CHANNEL_1;
+    ledc_channel.gpio_num = com_1;
+    ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
+
+    uint8_t duty_on=0;
+    uint8_t duty_off=0;
     while(1)
     {
-        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 15));
+        // Switch from COM1 to COM0
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty_mid));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty_high));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        // Segments
+        duty_on = duty_low;
+        duty_off = duty_high;
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty_on));
         ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0));
-        vTaskDelay(pdMS_TO_TICKS(2000));        
-        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 0));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7));
+        vTaskDelay(pdMS_TO_TICKS(10));
+        // Invert COM0
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty_low));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        // Segments
+        duty_on = duty_high;
+        duty_off = duty_low;
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty_on));
         ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0));
-        vTaskDelay(pdMS_TO_TICKS(2000));        
-        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 15));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5, duty_off));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7));
+        vTaskDelay(pdMS_TO_TICKS(10));
+        // Switch from COM0 to COM1
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, duty_mid));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty_high));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1));
+        // Segments
+        duty_on = duty_low;
+        duty_off = duty_high;
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty_off));
         ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0));
-        vTaskDelay(pdMS_TO_TICKS(2000));        
-        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, 31));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2));
+        //ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, duty_mid));
+        //ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6));
+        //ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7, duty_mid));
+        //ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7));
+        vTaskDelay(pdMS_TO_TICKS(10));
+        // Invert COM1
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, duty_low));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1));
+        // Segments
+        // Segments
+        duty_on = duty_high;
+        duty_off = duty_low;
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0, duty_off));
         ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_0));
-        vTaskDelay(pdMS_TO_TICKS(2000));        
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_1));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_2));
+        //ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3, duty_mid));
+        //ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_3));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_4));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_5));
+        ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6, duty_on));
+        ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_6));
+        //ESP_ERROR_CHECK(ledc_set_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7, duty_mid));
+        //ESP_ERROR_CHECK(ledc_update_duty(LEDC_HIGH_SPEED_MODE, LEDC_CHANNEL_7));
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 }
--- bare_lcd_2digit/src/lcd_voltage.h
+++ bare_lcd_2digit/src/lcd_voltage.h
@@ -7,17 +7,17 @@
 #include "driver/gpio.h"
 #include "driver/ledc.h"
 
-static const gpio_num_t com_1 = GPIO_NUM_23;
-static const gpio_num_t com_2 = GPIO_NUM_22;
+static const gpio_num_t seg_0 = GPIO_NUM_23;
+static const gpio_num_t seg_1 = GPIO_NUM_22;
+static const gpio_num_t seg_2 = GPIO_NUM_21;
+static const gpio_num_t seg_3 = GPIO_NUM_19;
+static const gpio_num_t seg_4 = GPIO_NUM_18;
+static const gpio_num_t seg_5 = GPIO_NUM_5;
+static const gpio_num_t seg_6 = GPIO_NUM_17;
+static const gpio_num_t seg_7 = GPIO_NUM_16;
 
-static const gpio_num_t seg_1 = GPIO_NUM_21;
-static const gpio_num_t seg_2 = GPIO_NUM_19;
-static const gpio_num_t seg_3 = GPIO_NUM_18;
-static const gpio_num_t seg_4 = GPIO_NUM_5;
-static const gpio_num_t seg_5 = GPIO_NUM_17;
-static const gpio_num_t seg_6 = GPIO_NUM_16;
-static const gpio_num_t seg_7 = GPIO_NUM_4;
-static const gpio_num_t seg_8 = GPIO_NUM_15;
+static const gpio_num_t com_0 = GPIO_NUM_4;
+static const gpio_num_t com_1 = GPIO_NUM_15;
 
 void lcd_voltage_task(void*);
 
