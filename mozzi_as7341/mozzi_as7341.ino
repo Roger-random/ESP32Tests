6a7b5a9d746a70e1d7b2c51d008217069dc6e0fd
Regorlas <roger.random@outlook.com>
2023-01-03 22:30:49 -0800

    Test sketch: LED double flash

--- mozzi_as7341/mozzi_as7341.ino
+++ mozzi_as7341/mozzi_as7341.ino
@@ -1,9 +1,14 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  pinMode(2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(100);
+  digitalWrite(2, HIGH);
+  delay(100);
+  digitalWrite(2, LOW);
+  delay(1000);
 }
